a7fb6aeb0e6a0e91b07d108f3e29da1b6dd8c271
eldensalassa <norriselden06@gmail.com>
2024-11-16 02:00:57 +0800

    Memperbaiki Logika Button pada Main Menu dan Memberikan Outline pada Text Button

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	# Menghubungkan tombol dengan fungsinya
+	print("Main Menu Ready!")
 	$Play.connect("pressed", Callable(self, "_on_play_button_pressed"))
 	$Knowledge.connect("pressed", Callable(self, "_on_knowledge_button_pressed"))
 	$Setting.connect("pressed", Callable(self, "_on_setting_button_pressed"))
@@ -9,15 +10,15 @@ func _ready():
 
 func _on_play_button_pressed():
 	print("Play button clicked")
-	get_tree().change_scene("res://Scenes/Game.tscn")  # Ganti dengan scene game Anda
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")  # Ganti dengan scene game Anda
 
 func _on_knowledge_button_pressed():
 	print("Knowledge button clicked")
-	get_tree().change_scene("res://Scenes/Knowledge.tscn")  # Ganti dengan scene knowledge Anda
+	get_tree().change_scene_to_file("res://Scenes/Knowledge.tscn")  # Ganti dengan scene knowledge Anda
 
 func _on_setting_button_pressed():
 	print("Setting button clicked")
-	get_tree().change_scene("res://Scenes/Setting.tscn")  # Ganti dengan scene setting Anda
+	get_tree().change_scene_to_file("res://Scenes/Setting.tscn")  # Ganti dengan scene setting Anda
 
 func _on_exit_button_pressed():
 	print("Exit button clicked")
